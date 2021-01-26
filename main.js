bcabbebede662e4470139e46c4dc16ae72e99c40
Arvind Kumawat <55478740+arvind37@users.noreply.github.com>
2021-01-26 22:20:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+
+function myFunction() {
+  var x = document.getElementById("main_menu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
